b76372ba3e5966487aefc1c1f9e8e035e066668d
paturrohmansiddik <paturrohmansiddik@gmail.com>
2024-09-21 19:02:58 +0700

    Add files via upload

---
+++ PATUR ROHMAN SIDDIK.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("NAMA  : PATUR ROHMAN SIDDIK \n");
+	printf("NIM   : 2441036 \n");
+	printf("PRODI : TEKNIK INFORMATIKA \n");
+	return 0;
+}
---
+++ PATUR.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main()
+{
+		int PANJANG, LEBAR, LUAS;
+		PANJANG = 10;
+		LEBAR = 7;
+		LUAS = PANJANG * LEBAR;
+		printf("Diketahui : Panjang = 10, lebar = 7");
+		printf("\nDitanya : Luas = P x L");
+		printf("\nHasilnya : %i", LUAS);
+		return 0;
+}
